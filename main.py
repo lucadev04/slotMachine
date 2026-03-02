a3d2e7ce0e99b1756aeb8ce585843084ca74cc0f
lucadev04 <luca07457@gmail.com>
2026-03-02 22:05:14 +0100

    implemented basic slot machine

---
+++ main.py
@@ -0,0 +1,38 @@
+import random as rd
+import time
+
+def main():
+    coins = 100
+    while True:
+        if coins == 0:
+            print("You lost all your money, haha")
+            exit()
+        slotQuery = input(f"Do you want to play(1 coin per throw)? You have {coins} coins(y/n)")
+        if slotQuery == "y":
+            for n in range(100):
+                randNum = rd.randint(1, 10)
+                randNum2 = rd.randint(1, 10)
+                randNum3 = rd.randint(1, 10)
+                print(f"\r[ {randNum} | {randNum2} | {randNum3} ]  ", end="", flush=True)
+                time.sleep(0.01+n/600)
+            print("\n")
+            randNums = [randNum, randNum2, randNum3]
+            randNums_sorted = sorted(randNums)
+            if randNums_sorted[0] == randNums_sorted[1] == randNums_sorted[2]:
+                coins += 100
+                print("You won (three equal numbers)")
+            elif randNums_sorted[0] == randNums_sorted[1] or randNums_sorted[1] == randNums_sorted[2]:
+                coins += 50
+                print("You won half (two equal numbers)")
+            else:
+                coins -= 1
+                print("You lost")
+        elif slotQuery == "n":
+            print("Good bye")
+            exit()
+        else:
+            print("You must put in y or n, lol")
+
+
+if __name__ == "__main__":
+    main()
